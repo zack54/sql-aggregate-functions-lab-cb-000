b1a62faa5a027cd403f8c8c9805017527d1c238b
zack54 <shninder5454@gmail.com>
2017-12-22 20:29:56 +0100

    Add aggregate functions

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,21 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "select max(gpa) from students;"
 end
 
 def lowest_student_gpa
+  "select min(gpa) from students;"
 end
 
 def average_student_gpa
+  "select avg(gpa) from students;"
 end
 
 def total_tardies_for_all_students
+  "select sum(tardies) from students;"
 end
 
 def average_gpa_for_9th_grade
+  "select avg(gpa) from students where grade = 9;"
 end
